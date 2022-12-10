3a510b51b7944a4b86360e3aa86f82bf8398479d
Kamil Ondrák <kamil.ondrak@deutsche-boerse.com>
2022-12-10 20:56:56 +0100

    Day 8 part 1

---
+++ src/bin/08.rs
@@ -0,0 +1,98 @@
+pub struct Tree {
+    height: u8,
+    visible: bool,
+}
+
+pub fn visible_left(trees: &[Vec<Tree>], x: usize, y: usize) -> bool {
+    let height = trees[x][y].height;
+    for i in (0..x).rev() {
+        if trees[i][y].height >= height {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn visible_up(trees: &[Vec<Tree>], x: usize, y: usize) -> bool {
+    let height = trees[x][y].height;
+    for i in (0..y).rev() {
+        if trees[x][i].height >= height {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn visible_right(trees: &[Vec<Tree>], x: usize, y: usize, size: usize) -> bool {
+    let height = trees[x][y].height;
+    for line in trees.iter().take(size).skip(x + 1) {
+        if line[y].height >= height {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn visible_down(trees: &[Vec<Tree>], x: usize, y: usize, size: usize) -> bool {
+    let height = trees[x][y].height;
+    for i in (y + 1)..size {
+        if trees[x][i].height >= height {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut trees = input.lines().map(|line| {
+        line.chars().map(|ch| {
+            Tree { height: ch.to_digit(10).unwrap() as u8, visible: false }
+        }).collect::<Vec<Tree>>()
+    }).collect::<Vec<Vec<_>>>();
+
+    let num_lines = trees.len();
+    let num_trees = trees[0].len();
+
+    for x in 0..num_lines {
+        for y in 0..num_trees {
+            if x == 0 || y == 0 || x == num_lines - 1 || y == num_trees - 1
+                || visible_left(&trees, x, y)
+                || visible_up(&trees, x, y)
+                || visible_right(&trees, x, y, num_trees)
+                || visible_down(&trees, x, y, num_lines)
+            {
+                trees[x][y].visible = true
+            }
+        }
+    }
+
+    let result = trees.iter().flatten().filter(|tree| { tree.visible }).count();
+    Some(result as u32)
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &aoc::read_file("inputs", 8);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = aoc::read_file("examples", 8);
+        assert_eq!(part_one(&input), Some(21));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = aoc::read_file("examples", 8);
+        assert_eq!(part_two(&input), None);
+    }
+}

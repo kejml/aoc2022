c9d5b049de70b7130fec4a20ddc13e85a4c6c3f6
Kamil Ondrák <kamil.ondrak@deutsche-boerse.com>
2022-12-11 00:27:25 +0100

    Day 9 part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,4 +1,6 @@
+use std::cell::RefCell;
 use std::collections::HashSet;
+use std::ops::Deref;
 
 pub fn signum(x: i32) -> i32 {
     match x {
@@ -13,6 +15,21 @@ struct Rope {
     head_y: i32,
     tail_x: i32,
     tail_y: i32,
+}
+
+struct Knot {
+    x: i32,
+    y: i32,
+}
+
+impl Clone for Knot {
+    fn clone(&self) -> Self {
+        Knot { x: self.x, y: self.y }
+    }
+}
+
+struct LongRope {
+    knots: Vec<RefCell<Knot>>,
 }
 
 impl Rope {
@@ -45,9 +62,48 @@ impl Rope {
     }
 }
 
+impl LongRope {
+    fn left(&mut self) -> (i32, i32) {
+        self.knots[0].borrow_mut().x -= 1;
+        self.move_tail()
+    }
+
+    fn right(&mut self) -> (i32, i32) {
+        self.knots[0].borrow_mut().x += 1;
+        self.move_tail()
+    }
+
+    fn up(&mut self) -> (i32, i32) {
+        self.knots[0].borrow_mut().y -= 1;
+        self.move_tail()
+    }
+
+    fn down(&mut self) -> (i32, i32) {
+        self.knots[0].borrow_mut().y += 1;
+        self.move_tail()
+    }
+
+    fn move_tail(&mut self) -> (i32, i32) {
+        for i in 1..self.knots.len() {
+            self.knots[i].borrow_mut().move_knot(self.knots[i - 1].borrow().deref());
+        }
+        (self.knots.last().unwrap().borrow().x, self.knots.last().unwrap().borrow().y)
+    }
+}
+
+impl Knot {
+    fn move_knot(&mut self, parent: &Knot) -> (i32, i32) {
+        if (self.x - parent.x).abs() > 1 || (self.y - parent.y).abs() > 1 {
+            self.y -= signum(self.y - parent.y);
+            self.x -= signum(self.x - parent.x);
+        }
+        (self.x, self.y)
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let mut visited = HashSet::<(i32, i32)>::new();
-    visited.insert((0,0));
+    visited.insert((0, 0));
     let mut rope = Rope { head_x: 0, head_y: 0, tail_x: 0, tail_y: 0 };
 
     input.lines().for_each(|command| {
@@ -69,7 +125,24 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut visited = HashSet::<(i32, i32)>::new();
+    let mut rope = LongRope { knots: vec!(RefCell::new(Knot { x: 0, y: 0 }); 10) };
+    input.lines().for_each(|command| {
+        let mut iter = command.split(' ');
+        let direction = iter.next().unwrap().chars().next().unwrap();
+        let num_moves = iter.next().unwrap().parse::<i32>().unwrap();
+        for _i in 0..num_moves {
+            let tail_pos = match direction {
+                'R' => { rope.right() }
+                'L' => { rope.left() }
+                'U' => { rope.up() }
+                'D' => { rope.down() }
+                _ => panic!("Unexpected input")
+            };
+            visited.insert(tail_pos);
+        }
+    });
+    Some(visited.len() as u32)
 }
 
 fn main() {
@@ -91,6 +164,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = aoc::read_file("examples", 9);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(1));
     }
 }
